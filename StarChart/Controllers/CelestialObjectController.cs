e14c055316ee8d036df3618af75ec904909bc461
Asanka <arajapakse@billtrust.com>
2018-11-24 06:06:36 -0500

    Completed the module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
 
 namespace StarChart.Controllers
@@ -11,6 +12,52 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects.SingleOrDefault(c => c.Id == id);
+
+            if (celestialObject == null)
+            {
+                return NotFound();
+            }
+
+            celestialObject.OrbitedObjectId = _context.CelestialObjects.SingleOrDefault(o =>
+                o.OrbitedObjectId != null && o.OrbitedObjectId == celestialObject.Id)?.OrbitedObjectId;
+
+            return Ok(celestialObject);
+        }
+
+        [HttpGet("name")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObject = _context.CelestialObjects.SingleOrDefault(c => c.Name == name);
+
+            if (celestialObject == null)
+            {
+                return NotFound();
+            }
+
+            celestialObject.OrbitedObjectId = _context.CelestialObjects.SingleOrDefault(o =>
+                o.OrbitedObjectId != null && o.OrbitedObjectId == celestialObject.Id)?.OrbitedObjectId;
+
+            return Ok(celestialObject);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestialObjects = _context.CelestialObjects.ToList();
+
+            foreach (var celestialObject in celestialObjects)
+            {
+                celestialObject.OrbitedObjectId = _context.CelestialObjects.SingleOrDefault(o =>
+                    o.OrbitedObjectId != null && o.OrbitedObjectId == celestialObject.Id)?.OrbitedObjectId;
+            }
+
+            return Ok(celestialObjects);
         }
     }
 }
